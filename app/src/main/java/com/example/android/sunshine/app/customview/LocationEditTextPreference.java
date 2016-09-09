71ab02986a09a88d545b3a85d50520c6fa7c15d1
Edmund Johnson <edmund@jumpingmouse.uk>
2016-09-09 10:41:42 +0100

    Add minimum length validation for location setting.

--- app/src/main/java/com/example/android/sunshine/app/customview/LocationEditTextPreference.java
+++ app/src/main/java/com/example/android/sunshine/app/customview/LocationEditTextPreference.java
@@ -1,20 +1,32 @@
 package com.example.android.sunshine.app.customview;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.android.sunshine.app.R;
 
+
 /**
+ * Custom EditTextPreference with validation added.
  * @author Edmund Johnson
  */
 public class LocationEditTextPreference extends EditTextPreference {
 
-    private static final int DEFAULT_MINIMUM_LOCATION_LENGTH = 1;
+    private static final int DEFAULT_MINIMUM_LOCATION_LENGTH = 2;
 
     private int mMinLength;
+
+    // The field containing the location
+    private EditText mEditText;
 
     /**
      * Constructor.
@@ -33,15 +45,54 @@ public class LocationEditTextPreference extends EditTextPreference {
         }
     }
 
+    /**
+     * Shows the dialog associated with this Preference. This is normally initiated
+     * automatically on clicking on the preference. Call this method if you need to
+     * show the dialog on some other event.
+     *
+     * @param state Optional instance state to restore on the dialog
+     */
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+        mEditText = getEditText();
+
+        mEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Dialog dialog = getDialog();
+                if (dialog instanceof AlertDialog) {
+                    AlertDialog alertDialog = (AlertDialog) dialog;
+                    Button positiveButton = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+
+                    if (mEditText.getText() == null
+                            || mEditText.getText().length() < getMinLength()) {
+                        positiveButton.setEnabled(false);
+                    } else {
+                        positiveButton.setEnabled(true);
+                    }
+                }
+            }
+        });
+    }
+
     // Getters and setters
 
     public int getMinLength() {
         return mMinLength;
     }
-    public void setMinLength(int minLength) {
-        this.mMinLength = minLength;
-//        invalidate();
-//        requestLayout();
-    }
+//    public void setMinLength(int minLength) {
+//        this.mMinLength = minLength;
+////        invalidate();
+////        requestLayout();
+//    }
 
 }
